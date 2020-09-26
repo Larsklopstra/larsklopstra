76ab845a62e9d54406b0b5826bb5bd67dd55bc11
Lars Klopstra <larsklopstra@gmail.com>
2020-09-26 16:26:35 +0200

    Update config.fish

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,17 +1,11 @@
+set PATH $PATH ~/.config/composer/vendor/bin
+
 function art
 	php artisan $argv
 end
 
 function homestead
-	cd ~/Homestead
-	
-	if test $argv -z
-		vagrant up
-	else if test $argv[1] = 'r'
-		vagrant reload -p
-	else if test $argv[1] = 'edit'
-		gedit ./Homestead.yaml
-	else
-		vagrant $argv
-	end
+	cd ~/Homestead && vagrant $argv
 end
+
+starship init fish | source
